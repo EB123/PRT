18eace1708cc4280e5d1f9f23a2fb9cb1329ebcf
EB123 <eyal.brave@gmail.com>
2016-08-18 18:19:47 +0300

    added active_worker counter to index view

--- prt_ui/ui/views.py
+++ prt_ui/ui/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render
 import zmq
 
 
-address = "127.0.0.1"
-port = "4141"
+#address = "127.0.0.1"
+#port = "4141"
+
+def create_zmq_connection(address, port, socket_type): # TODO - should be taken from prt_utils
+    context = zmq.Context()
+    socket = context.socket(socket_type)
+    socket.connect("tcp://%s:%s" % (address, port))
+    return socket
+
 
 def index(request):
-    context = {}
+    socket = create_zmq_connection("127.0.0.1", "5553", zmq.REQ)
+    socket.send_json(["prm", "active_proxy_workers"])
+    resp = socket.recv_json()
+    context = {'active_workers': resp}
     return render(request, "ui/index.html", context)
 
 """
@@ -15,13 +25,12 @@ def ajax_get_num_proxy_workers(request):
     if request.method == "POST" and request.is_ajax():
 """
 
-def create_zmq_connection(address, port):
-    context = zmq.Context()
-    socket = context.socket(zmq.PAIR)
-    socket.connect("tcp://%s:%s" % (address, port))
-    return socket
 
+
+
+"""
 def send_ajax_from_ui(request):
     if request.method == "POST" and request.is_ajax():
         create_zmq_connection(address, port)
+"""
 
