8532510a112ddc1fe8ce06bb06afc3de9b83d2d2
EB123 <eyal.brave@gmail.com>
2016-09-01 11:08:17 +0300

    sproxy

--- sproxy.py
+++ sproxy.py
@@ -1,11 +1,18 @@
 __author__ = 'Eyal'
 import time
-
+import paramiko
+import atexit
+import prt_utils
+import os
+import requests
 
 class sProxy:
 
-    def __init__(self, name):
+    def __init__(self, name, port='8080', user='root'):
         self.name = name
+        self.user = user
+        self.sshconn = self._sshconnect(name, user)
+        self.port = port
         # TODO - self.fqdn
 
     def __str__(self):
@@ -14,15 +21,34 @@ class sProxy:
     def __repr__(self):
         return self.name
 
+    def _sshconnect(self, name, user):
+        try:
+            sshconn = paramiko.SSHClient()
+            sshconn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            sshconn.connect(name, username=user)
+            atexit.register(paramiko.client.SSHClient.close, sshconn)
+            return sshconn
+        except Exception: # TODO - specific exceptions
+            raise # TODO - consider adding a logger to the class
+
     def dump_cache(self):
         # TODO - Create dump_cache func
         time.sleep(1)
         return "Dumping Cache"
 
-    def check_dump_age(self):
+    def check_dump_age(self, dump_location = '/workspace/repository/proxy/dump'):
         # TODO - Create check_dump_age func
-        time.sleep(1)
-        return 30
+        cmd = '/bin/ls -l ' + dump_location + " | grep dump | grep rdy$ | awk '{print $9}'"
+        exitCode, output, error = prt_utils.ssh_execute(self.sshconn, cmd)
+        if exitCode == 0 and len(output) == 1: # TODO - better way for handling errors
+            dump_file = os.path.join(dump_location, output[0])
+            cmd = "stat --printf '%%Y' %s" % dump_file
+            exitCode, dump_last_modified, error = prt_utils.ssh_execute(self.sshconn, cmd)
+            if exitCode == 0 and len(dump_last_modified) == 1:
+                dump_last_modified = int(dump_last_modified[0])
+                now = time.time()
+                dump_age = float(now - dump_last_modified) / 3600
+                return dump_age
 
     def stop_proxy(self):
         # TODO - Create stop_proxy func
@@ -39,22 +65,35 @@ class sProxy:
         time.sleep(10)
         return "Proxy %s Started" % self.name
 
-    def check_proxy_state(self):
+    def check_state(self):
         # TODO - Create check_proxy_state func
-        time.sleep(1)
-        return "State Checked"
+        an_status_path = '/proxy/keepalive?file=/workspace/temp/1'
+        lb_status_path = '/proxy/keepalive?file=/workspace/temp/1.txt'
+        paths = {'an_status': an_status_path, 'lb_status': lb_status_path}
+        status = {}
+        for path in paths:
+            url = 'http://%s:%s%s' % (self.name, self.port, paths[path])
+            response = requests.get(url) # TODO - add timeout
+            status[path] = response.text.strip() # TODO - check status_code
+        return status
 
-    def in_rotation(self):
+    # TODO - Create decorator that validates that action is 'in' or 'out' and that type is 'an' or 'lb'
+    def in_out_rotation(self, proxy_type, action):
         # TODO - Create in_rotation func
-        time.sleep(1)
-        return "Proxy is in rotation"
+        actions = {'an': {'in': '1', 'out': '0'}, 'lb': {'in': 'ppp', 'out': 'rrr'}}
+        scripts = '/workspace/test/proxy/peer39-proxy/WEB-INF/scripts'
+        change_rotation_script = 'setValue.sh'
+        keepalive_url = '/workspace/temp/1'
+        if proxy_type == 'lb':
+            keepalive_url = keepalive_url + '.txt'
+        cmd = '. ./.paramiko_profile ; cd %s && ./%s %s %s' % (scripts, change_rotation_script, keepalive_url, actions[proxy_type][action])
+        exitCode, output, error = prt_utils.ssh_execute(self.sshconn, cmd)
+        return exitCode, output, error
 
+    """
     def out_rotation(self):
         # TODO - Create out_rotation func
         time.sleep(1)
         return "Proxy is out of rotation"
+    """
 
-    def check_state(self):
-        # TODO - Create check_state func
-        time.sleep(1)
-        return "Stopped"
